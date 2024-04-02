708c2ebbad9b344f25eb5d6c7dfd1960ac618837
SK <sk@outzource.dk>
2024-04-02 14:33:47 +0200

    ViewBag

--- src/ComicBookStore/ComicBookStore/Controllers/ComicBookController.cs
+++ src/ComicBookStore/ComicBookStore/Controllers/ComicBookController.cs
@@ -8,5 +8,26 @@ namespace ComicBookStore.Controllers
 {
     public class ComicBookController : Controller
     {
+        public ActionResult Detail()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday) {
+                return Redirect ("/");
+            }
+
+
+            ViewBag.Title = "Super Mario 64";
+            ViewBag.Description = "Super Mario 64 is a 1996 platform video game developed and published by Nintendo for the Nintendo 64.";
+            ViewBag.Characters = new string[]
+            {
+                "Mario",
+                "Princess Peach",
+                "Bowser",
+                "Toad",
+                "Yoshi"
+            };
+
+            return View();
+            //return Content ("Hello from the Comic Book Store");
+        }
     }
 }
