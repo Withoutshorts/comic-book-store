fc98adbf19244bc5838122c4c0044dd15c823fa0
SK <sk@outzource.dk>
2024-04-03 08:54:49 +0200

    Model

---
+++ src/ComicBookStore/ComicBookStore/Models/ComicBooks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookStore.Models
+{
+    public class ComicBooks
+    {
+
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public string Description { get; set; }
+        public string Artists { get; set; }
+
+        public bool Favorite { get; set; }
+    }
+}
